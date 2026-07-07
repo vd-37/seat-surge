5a4e71ad68c3be0da02c2b7cb6606e4b912045f3
vd-37 <vinayakadayanand007@gmail.com>
2026-07-08 01:17:48 +0530

    Initial commit for seat-surge

---
+++ src/main/java/com/vd/seatsurge/SeatSurgeApplication.java
@@ -0,0 +1,13 @@
+package com.vd.seatsurge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatSurgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatSurgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vd/seatsurge/SeatSurgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vd.seatsurge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatSurgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
